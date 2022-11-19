0e805b58214729fbc5d506930525f6eb479bee28
«Roman <research777@mail.ru>
2022-11-19 23:15:18 +0300

    DRSEMINAR3_Ex19

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@
 
 //Задача 21
 
+void Pol5Dig(int num)
+{
+    int current = num / 1000; 
+    
+    if (current / 100 == 0)
+    {
+        if ((num / 10000) == (num % 10) && (current % 10) == (num % 100) / 10)
+        {
+            Console.WriteLine($"The number {num} is a polyndrom");
+        }
+        else Console.WriteLine($"The number {num} is not a polydrome");
+    }else Console.WriteLine($"Number {num} is not five digits!");
+}
+
+Console.WriteLine("Enter a five-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Pol5Dig(number);
+
+
+
 //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
 //A (3,6,8); B (2,1,-7), -> 15.84
